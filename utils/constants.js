7106b6d5708f93a0bb76bd1675eb2d7efb2681f4
David Biagiola <@david.biagiola>
2024-04-26 10:55:27 -0400

    first commit

---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT = "https://misty-thrilling-rain.solana-devnet.quiknode.pro/b0758030b4fb8880e81a61ab334ab40873a36f6c/";
+
+// Change this with your candy machine id
+export const CANDY_MACHINE_ID = new PublicKey("APDPkmr6dS2E4sCH6jVtFGX7dzuAbVnZkokikN4kyFiU");
